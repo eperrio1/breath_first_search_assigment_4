461fe7f3708f85c501321b001d026ea13e1deff3
eperrio1 <112588781+eperrio1@users.noreply.github.com>
2022-10-25 17:43:28 -0500

    updated Magic Kingdom.txt and presents the user with a list of node to choose from

--- main.py
+++ main.py
@@ -3,24 +3,36 @@ from collections import deque
 def main():
     print('\n\n========  Breadth-first search  ========\n\n')
 
-    again = True
-
-    while again == True:
+    bool = True
+    while bool == True:
         file = input("Select data file: Magic Kingdom.txt or Hollywood Studios.txt: ")
         while file != "Magic Kingdom.txt" and file != "Hollywood Studios.txt":
             file = input("Select data file: Magic Kingdom.txt or Hollywood Studios.txt: ")
 
         graph = map(file)
 
-        print(f'Map: {graph}')
-        print(bfs(graph))
+        print(f'\nList of nodes:\n{list_nodes(file)}\n ')
+        print(f'\nThe path from start to fisnish:\n{bfs(graph)}\n')
 
-        again = (input('Would you like to fine another path? Yes or No: ').lower())
-        if again == "no":
-            again = False
-        if again == "yes":
-            again = True
+        play_again = ''
+        while play_again != "no" and play_again != "yes":
+            play_again = (input('Would you like to find another path? Yes or No: ').lower())
+            if play_again == "no":
+                bool = False
+            if play_again == "yes":
+                bool = True
 
+def list_nodes(data_file):
+    with open(data_file, 'r') as file:
+        nodes_list = []
+        data = file.readlines()
+        for line in data:
+            line_list = line.strip().split(',')
+            for node in line_list:
+                node = node.strip()
+                if node not in nodes_list:
+                    nodes_list.append(node)
+        return nodes_list
 
 
 def map(data_file):
@@ -40,49 +52,73 @@ def map(data_file):
             for value in route[1:]:
                 values.append(value.replace('\n', '').strip().lower())
             connections[key] = values
-
     return connections
 
 
 #TODO: Update so it keeps track of a clear path from start to finish
+
 def bfs(graph):
     start = ''
     end = ''
     while start not in graph or end not in graph:
         start = input("Where should the search START?: ").lower()
         end = input("Where should the search END?: ").lower()
-        if start not in graph:
+        while start not in graph:
             print("Sorry, that location is not in the graph, please input a new starting point. ")
             start = input("Where should the search START?: ").lower()
-
-        if end not in graph:
+        while end not in graph:
             print("Sorry, that location is not in the graph, please input a new ending point. ")
             end = input("Where should the search END?: ").lower()
 
+
+
+
+
     # list of nodes we've seen
     seen = []
+    path = []
+
     # creates queue with the starting node
     queue = deque([start])
+
 
     # Currently just checking that we are viewing all the nodes in the path
     while queue:
         # gets the first element in the queue
         node = queue.popleft()
+        print(f'The current node: {node}')
+
         # if you reached the end, ends the while loop
         if node == end:
             seen.append(node)
+            path.append(node)
             return seen
+
+
+
         # node not in the seen list, append the node
         if node not in seen:
             seen.append(node)
-        # loop through for the specific node
-        for i in graph[node]:
-            #if the elements in that current node is not in seen, append it to the queue
-            if i not in seen:
-                queue.append(i)
+            path.append(node)
+            print(f'The current seen: {seen}')
+            print(f'The current path: {path}')
 
-    # return the list of nodes we seen
-    return seen
+
+        # loop through for the specific node
+        for link in graph[node]:
+            print(f'The node attached to the current node: {graph[node]}')
+            print()
+
+            #if the elements in that current node is not in seen, append it to the queue
+            if link not in seen:
+                queue.append(link)
+                print(f"The current queue: {queue}\n")
+
+
+    return f'A path does not exist'
+
+
+
 
 if __name__ == "__main__":
     main()
